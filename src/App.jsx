d71f49eff461b20ce256cf255f1c1e4789623656
Levi <imansamiemosleh@gmail.com>
2025-07-06 11:57:57 +0200

    Restructuring the programm and adding root structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 
 import { Route, Routes } from 'react-router'
 import './App.css'
+import HomePage from './pages/HomePage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import SignInPage from './pages/SignInPage.jsx'
+import CreateEventPage from './pages/CreateEventPage.jsx'
+import EventDetailsPage from './pages/EventDetailsPage.jsx'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   
-
   return (
     <>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<p>Home</p>}/>
-        <Route path='/about' element={<p>about</p>}/>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/signup' element={<SignUpPage />}/>
+        <Route path='/signin' element={<SignInPage />}/>
+        <Route path='/create-event' element={<CreateEventPage />}/>
+        <Route path='/event' element={<EventDetailsPage />}/>
       </Routes>
+      <Footer />
     </>
   )
 }
